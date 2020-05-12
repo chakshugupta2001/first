afe1c750ccff8757ededf0bdb3da81f40d4d33f7
chakshu <chakshugupta2001@gmail.com>
2020-05-12 12:01:30 +0530

    initialize

---
+++ yaho.py
@@ -0,0 +1,17 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> for i in range(0,10):
+	print("yahoo")
+
+	
+yahoo
+yahoo
+yahoo
+yahoo
+yahoo
+yahoo
+yahoo
+yahoo
+yahoo
+yahoo
+>>> 
