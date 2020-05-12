337e7a1fd1657e9ed9ecc998ff8e923c2a31aff1
chakshu <chakshugupta2001@gmail.com>
2020-05-12 12:33:56 +0530

    maa modified

--- maa.py
+++ maa.py
@@ -2,4 +2,6 @@ Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit 
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> print("jai maa")
 jai maa
+>>> print("ganesh")
+ganesh
 >>> 
